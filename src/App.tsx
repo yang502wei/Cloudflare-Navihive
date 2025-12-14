12471575782ab479b4530bebd11dc6ccd3bf27e3
yang502wei <109737310+yang502wei@users.noreply.github.com>
2025-12-14 20:02:20 +0800

    Remove GitHub icon and link from App component
    Removed GitHub icon and link from the bottom of the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1775,39 +1775,6 @@ function App() {
           </Dialog>
 
           {/* GitHub角标 - 在移动端调整位置 */}
-          <Box
-            sx={{
-              position: 'fixed',
-              bottom: { xs: 8, sm: 16 },
-              right: { xs: 8, sm: 16 },
-              zIndex: 10,
-            }}
-          >
-            <Paper
-              component='a'
-              href='https://github.com/zqq-nuli/Navihive'
-              target='_blank'
-              rel='noopener noreferrer'
-              elevation={2}
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                p: 1,
-                borderRadius: 10,
-                bgcolor: 'background.paper',
-                color: 'text.secondary',
-                transition: 'all 0.3s ease-in-out',
-                '&:hover': {
-                  bgcolor: 'action.hover',
-                  color: 'text.primary',
-                  boxShadow: 4,
-                },
-                textDecoration: 'none',
-              }}
-            >
-              <GitHubIcon />
-            </Paper>
-          </Box>
         </Container>
       </Box>
     </ThemeProvider>
